703a88ee678dcfd9aa358bb8df41ab7395bfd4c4
Arda <ardamutlu20@gmail.com>
2026-04-09 17:58:03 +0300

    add module federation manifest

--- apps/host/src/main.ts
+++ apps/host/src/main.ts
@@ -1,9 +1,14 @@
-import { registerRemotes } from '@module-federation/enhanced/runtime';
+import {registerRemotes} from '@module-federation/enhanced/runtime';
 
-fetch('/assets/module-federation.manifest.json')
+const manifestFile =
+  process.env.NODE_ENV === 'production'
+    ? '/assets/module-federation.manifest.prod.json'
+    : '/assets/module-federation.manifest.json';
+
+fetch(manifestFile)
   .then((res) => res.json())
   .then((remotes: Record<string, string>) =>
-    Object.entries(remotes).map(([name, entry]) => ({ name, entry })),
+    Object.entries(remotes).map(([name, entry]) => ({name, entry})),
   )
   .then((remotes) => registerRemotes(remotes))
   .then(() => import('./bootstrap').catch((err) => console.error(err)));
